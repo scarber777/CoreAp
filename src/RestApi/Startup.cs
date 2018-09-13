f5c1663c2b95e6c1fb8b8835c84f8f63ab255356
Shawn Deggans <shawn.deggans@catapultsystems.com>
2018-09-13 13:46:41 -0500

    Initial commit

---
+++ src/RestApi/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutoMapper.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RestApi
+{
+    public class Startup
+    {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Adding the injection
+
+
+
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ test/RestApiTest/AsAFeatureUserIWant.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RestApiTest
+{
+    public class AsAFeatureUserIWant
+    {
+        [Fact]
+        public void ToTellTheFeatureToCommitAnAction()
+        {
+            Assert.True(true);
+        }
+    }
+}
